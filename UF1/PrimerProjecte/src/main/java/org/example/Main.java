4416b4a76c94ee234717c5f5a1350a27e8bc6458
omarinese <oriolmarine12@gmail.com>
2023-09-19 13:22:35 +0200

    hola

---
+++ UF1/PrimerProjecte/src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Vaig a mostrar per la pantalla el missatge hello world
+
+        System.out.println("Hello world");
+        System.out.print("Hello world ");
+        System.out.println("Hello world");
+
+        //Comentar linees
+    }
+}
